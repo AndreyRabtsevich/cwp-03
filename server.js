afdd810215abe0cb8fc85a7ac4c239afda1050f7
Andrey <rabtsevichandrey11@gmail.com>
2018-10-03 23:14:52 +0300

    last changes

--- server.js
+++ server.js
@@ -1,33 +1,80 @@
 const net = require('net');
 const fs = require('fs');
+const path = require('path');
 const port = 8124;
+
+const reqFiles = 'FILES';
+const reqQA = 'QA';
+const resGood = 'ACK';
+const resBad = 'DEC';
+const resFiles = 'NEXT';
+const EOF = 'EOF';
+const defaultDir = process.env.FILES_DIR;
+const maxConn = parseInt(process.env.CONN);
+
 let seed = 0;
-const logger = fs.createWriteStream('client_id.txt');
+let arr = require('./qa.json')
+let clients = [];
+let files = [];
+let fdFile;
+let flag = 0;
+let connections = 0;
+
 
 const server = net.createServer((client) => {
-    client.id = Date.now() + seed++;
-    logger.write('client ' + client.id + ' connected\n');
+    let fil;
+    if(++connections === maxConn){
+        client.destroy();
+    }
     client.setEncoding('utf8');
 
     client.on('data', (data) => {
-        if (data == 'QA') {
-            client.write('ACK');
+        if (data === reqFiles || data === reqQA) {
+            client.id = getUniqID();
+            if (data === reqFiles) {
+                files[client.id] = [];
+                fs.mkdir(defaultDir + path.sep + client.id,(err)=>{
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                });
+            }
+            console.log(data);
+            console.log(`Client ${client.id} connected`);
+            clients[client.id] = data;
         }
-        else {
-            let answer = getRandom();
-            logger.write('; answer: ' + answer + '\n');
-            client.write(answer);
+        else if (client.id === undefined) {
+            client.write(resBad);
+            client.destroy();
+        }
+        if (clients[client.id] === reqFiles && data !== reqFiles) {
+            files[client.id].push(data);
+            flag++;
+            if (flag === 2) {
+                let buf = Buffer.from(files[client.id][0],'hex');
+                let filePath = defaultDir+path.sep+client.id+path.sep+files[client.id][1];
+                fil = fs.createWriteStream(filePath);
+                fil.write(buf);
+                flag=0;
+                files[client.id] = [];
+                fil.close();
+                client.write(resFiles);
+            }
         }
     });
 
     client.on('end', () => {
-        logger.write('client ' + client.id + ' disconnected\n');
+        connections--;
+        console.log(`Client ${client.id} disconnected`);
     });
 });
+
 server.listen(port, () => {
     console.log(`Server listening on localhost:${port}`);
 });
 
-function getRandom() {
-    return `${Math.floor(Math.random() * 2)}`;
+function getUniqID() {
+    return Date.now() + seed++;
 }
+
